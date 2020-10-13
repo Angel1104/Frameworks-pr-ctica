6619a7b55be7196f2bc5090e5cfd64914ba79c92
Angel1104 <71400131+Angel1104@users.noreply.github.com>
2020-10-12 20:54:29 -0400

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'login',
+  templateUrl: './index.html',
+  styleUrls: ['./styles.css']
+})
+export class login{
+    constructor(){
+        console.log("componente cargado")
+    }
+}
